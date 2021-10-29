47277e50ce39844179ba293a9e44581d34966683
Douglas Motta <tibeca@gmail.com>
2021-10-29 10:08:43 +0100

    Adiciona projeto node de backend que gera o token JWT

---
+++ auth-jwt/src/routes.js
@@ -0,0 +1,21 @@
+const Router = require('koa-router')
+
+// middlewares
+const error = require('./middlewares/error')
+const authenticated = require('./middlewares/auth')
+
+// handlers
+const users = require('./handlers/users')
+const auth = require('./handlers/auth')
+
+const router = new Router()
+
+router.use(error)
+
+router.get('/users/:id', authenticated, users.findOne)
+router.get('/users', authenticated, users.getAllUsers)
+router.post('/users', authenticated, users.createUser)
+
+router.post('/auth', auth.authenticate)
+
+module.exports = router
---
+++ auth-jwt/src/services/token.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken')
+
+const { crypto: config } = require('../../config')
+
+const signOptions = {
+  algorithm: 'RS256',
+  expiresIn: '1m'
+}
+
+const sign = payload => jwt.sign(payload, config.jwt.privateKey, signOptions)
+
+const verify = token => new Promise((resolve, reject) => jwt.verify(
+  token, 
+  config.jwt.publicKey,
+  (error, data) => error ? reject(error) : resolve(data)
+  ))
+
+module.exports = {
+  sign,
+  verify,
+}
